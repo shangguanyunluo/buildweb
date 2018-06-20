145e6996ee0ae8911d1324a440373019914a2716
shangguanyunluo <zhang.yafeng@foxmail.com>
2018-06-20 14:08:49 +0800

    project start
    Signed-off-by: shangguanyunluo <zhang.yafeng@foxmail.com>

---
+++ src/main/java/com/lenovo/cloudbuild/CloudbuildApplication.java
@@ -0,0 +1,12 @@
+package com.lenovo.cloudbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudbuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudbuildApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lenovo/cloudbuild/CloudbuildApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lenovo.cloudbuild;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudbuildApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
